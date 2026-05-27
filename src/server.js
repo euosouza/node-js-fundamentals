977b15d0c5d65a0a3ea7c6701a03abc49aadd8d3
Victor Souza <euosouza@gmail.com>
2026-05-27 18:27:49 -0300

    feat: adiciona instancia do servidor

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+import http from "node:http";
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    return res.end(`Servidor rodando em http://localhost:${PORT}`)
+});
+
+server.listen(PORT);
